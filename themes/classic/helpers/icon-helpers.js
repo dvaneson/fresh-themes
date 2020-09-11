a5fac8db0bf31cd4370baef0f7f442906765db14
Donovan Ellison <dvaneson@gmail.com>
2020-09-11 14:31:19 -0700

    Created helpers to select an icon based on a label

---
+++ themes/classic/helpers/icon-helpers.js
@@ -0,0 +1,17 @@
+(function() {
+    const printf = require('printf');
+
+    // Block helper function definitions.
+    var IconHelpers = module.exports = {
+  
+      // Returns a span for the icon specified by the label.
+      getIcon: function(label, options) {
+        let themeObj = options.data.root.opts.themeObj
+        let icons = themeObj.icons
+        console.log("icons: ", icons)
+        let icon = (icons && icons[label]) || "icon-link"
+        return printf('<span class="%s"></span>', icon);
+      }
+    };
+  
+  }).call(this);
