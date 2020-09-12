5606edeeebb38e6889eb88ecc64e7ba6f41b90da
Donovan Ellison <dvaneson@gmail.com>
2020-09-11 18:06:51 -0700

    Renamed getIcon to iconSpan and made it global

--- themes/classic/helpers/icon-helpers.js
+++ themes/classic/helpers/icon-helpers.js
@@ -3,14 +3,6 @@
 
     // Block helper function definitions.
     var IconHelpers = module.exports = {
-  
-      // Returns a span for the icon specified by the label.
-      getIcon: function(label, options) {
-        let themeObj = options.data.root.opts.themeObj
-        let icons = themeObj.icons
-        let icon = (icons && icons[label]) || "icon-link"
-        return printf('<span class="%s"></span>', icon);
-      },
 
       getIconPartial: function(label) {
         return printf("icon-%s", label)
